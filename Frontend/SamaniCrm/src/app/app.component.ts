3771bd535213f0c1487c1e9a10331f94f52e4eea
mr.samani <mr.samani1368@gmail.com>
2025-04-21 16:32:59 +0330

    open api generation

--- Frontend/SamaniCrm/src/app/app.component.ts
+++ Frontend/SamaniCrm/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SyncHistoryManagerServiceProxy } from '@shared/service-proxies';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SamaniCrm';
+
+
+  
+  constructor(
+    private syncHistoryManagerService: SyncHistoryManagerServiceProxy,
+) {
+}
+
+getAll() {
+    
+
+
+    this.syncHistoryManagerService
+        .getAll(
+            this.syncChainFilter,
+            this.user ? +this.user.id : undefined,
+            this.tenant ? this.tenant.id : undefined,
+            this.deviceManufacturerFilter,
+            this.deviceModelFilter,
+            this.versionOSFilter,
+            this.sdkNumberFilter,
+            undefined,
+            undefined,
+            this.syncGuidFilter,
+            undefined,
+            this.hasException,
+            this._dateTimeService.getStartOfDayForDate(this.dateRange[0]),
+            this._dateTimeService.getEndOfDayForDate(this.dateRange[1]),
+            this.primengTableHelper.getSorting(this.dataTable),
+            this.primengTableHelper.getMaxResultCount(this.paginator, event),
+            this.primengTableHelper.getSkipCount(this.paginator, event),
+        )
+        .pipe(finalize(() => this.primengTableHelper.hideLoadingIndicator()))
+        .subscribe((result) => {
+            this.primengTableHelper.totalRecordsCount = result.totalCount;
+            this.primengTableHelper.records = result.items;
+            this.primengTableHelper.hideLoadingIndicator();
+        });
+}
+
+
 }
