42f40fa234aa033de4ef60a1683f71ab32ac6b8f
mr.samani <mr.samani1368@gmail.com>
2025-05-11 10:29:04 +0330

    fix loop in run script

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
+setlocal
+set "currentDirector=%CD%"
 
+:MainMenu
 CLS
 ECHO ---- Mohammadreza Samani Angular & C#.net9 ----
 ECHO.
-set currentDirector=%CD%
-
 
 ECHO [1]. Start FrontEnd
 ECHO [2]. Run open api generation
@@ -25,63 +26,49 @@ IF ERRORLEVEL 2 GOTO RunOpenApiGen
 IF ERRORLEVEL 1 GOTO StartFrontEnd
 
 ::---------------------------------------------------------
-
 :ExitCMD
 ECHO *** Exit... ***
-call exit
-ECHO.
-GOTO End
+GOTO PromptClose
 
 ::---------------------------------------------------------
-
 :OpenVsCode
 ECHO *** Opening VS Code for frontend ***
-CD /D  %currentDirector%\Frontend\SamaniCrm
+CD /D "%currentDirector%\Frontend\SamaniCrm"
 call code .
-ECHO.
 GOTO End
-
 
 ::---------------------------------------------------------
 :UpdateDbWithSeed
 ECHO *** Run migrations on database with seed data ***
-CD /D  %currentDirector%\BackEnd
+CD /D "%currentDirector%\BackEnd"
 call seed-database.bat
-ECHO.
 GOTO End
 
 ::---------------------------------------------------------
 :RunOpenApiGen
 ECHO *** Generate Services and model from swagger ***
-CD /D  %currentDirector%\Frontend\SamaniCrm
+CD /D "%currentDirector%\Frontend\SamaniCrm"
 call npm run generate-api
-ECHO.
 GOTO End
-
 
 ::---------------------------------------------------------
 :StartFrontEnd
 ECHO *** Serve Frontend ***
-CD /D  %currentDirector%\Frontend\SamaniCrm
+CD /D "%currentDirector%\Frontend\SamaniCrm"
 call npm run start
-ECHO.
 GOTO End
 
-
-
-
+::---------------------------------------------------------
 :End
-setlocal
-:PROMPT
+GOTO PromptClose
+
+::---------------------------------------------------------
+:PromptClose
 SET /P AREYOUSURE=close (Y/[N])?
 IF /I "%AREYOUSURE%" EQU "Y" GOTO ENDApp
+GOTO MainMenu
 
-echo ...  ...
-
-
-:WaitApp
-cmd /k
-
-
+::---------------------------------------------------------
 :ENDApp
 endlocal
+exit
