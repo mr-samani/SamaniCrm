2114740f39e646b938ade964af2bfb6a91fdac95
mr.samani <mr.samani1368@gmail.com>
2025-06-11 07:40:36 +0330

    config start public site

--- run.bat
+++ run.bat
@@ -7,23 +7,27 @@ CLS
 ECHO ---- Mohammadreza Samani Angular & C#.net9 ----
 ECHO.
 
-ECHO [1]. Start FrontEnd
-ECHO [2]. Run open api generation
-ECHO [3]. Update database with seeds
-ECHO [4]. Open frontend in VScode
-ECHO [5]. Exit
+ECHO [1]. Start FrontEnd dashboard
+ECHO [2]. Start public site
+ECHO [3]. Run open api generation
+ECHO [4]. Update database with seeds
+ECHO [5]. Open dashboard in VScode
+ECHO [6]. Open public site in VScode
+ECHO [7]. Exit
 ECHO.
 
-CHOICE /C 12345 /M "Enter your choice:"
+CHOICE /C 1234567 /M "Enter your choice:"
 
 ECHO.
 
 :: Note - list ERRORLEVELS in decreasing order
-IF ERRORLEVEL 5 GOTO ExitCMD
-IF ERRORLEVEL 4 GOTO OpenVsCode
-IF ERRORLEVEL 3 GOTO UpdateDbWithSeed
-IF ERRORLEVEL 2 GOTO RunOpenApiGen
-IF ERRORLEVEL 1 GOTO StartFrontEnd
+IF ERRORLEVEL 7 GOTO ExitCMD
+IF ERRORLEVEL 6 GOTO OpenPublicVsCode
+IF ERRORLEVEL 5 GOTO OpenDashboardVsCode
+IF ERRORLEVEL 4 GOTO UpdateDbWithSeed
+IF ERRORLEVEL 3 GOTO RunOpenApiGen
+IF ERRORLEVEL 2 GOTO StartPublicSite
+IF ERRORLEVEL 1 GOTO StartDashboardFrontEnd
 
 ::---------------------------------------------------------
 :ExitCMD
@@ -31,11 +35,18 @@ ECHO *** Exit... ***
 GOTO PromptClose
 
 ::---------------------------------------------------------
-:OpenVsCode
-ECHO *** Opening VS Code for frontend ***
+:OpenPublicVsCode
+ECHO *** Opening VS Code for frontend PUBLIC SITE***
+CD /D "%currentDirector%\Frontend\SamaniPublicSite"
+call code .
+GOTO End
+::---------------------------------------------------------
+:OpenDashboardVsCode
+ECHO *** Opening VS Code for frontend DASHBOARD***
 CD /D "%currentDirector%\Frontend\SamaniCrm"
 call code .
 GOTO End
+
 
 ::---------------------------------------------------------
 :UpdateDbWithSeed
@@ -52,12 +63,18 @@ call npm run generate-api
 GOTO End
 
 ::---------------------------------------------------------
-:StartFrontEnd
-ECHO *** Serve Frontend ***
+:StartDashboardFrontEnd
+ECHO *** Serve Frontend Dashboard***
 CD /D "%currentDirector%\Frontend\SamaniCrm"
 call npm run start
 GOTO End
 
+::---------------------------------------------------------
+:StartPublicSite
+ECHO *** Serve Frontend Public site ***
+CD /D "%currentDirector%\Frontend\SamaniPublicSite"
+call npm run start
+GOTO End
 ::---------------------------------------------------------
 :End
 GOTO PromptClose
